b609cf6e8691c98a0b565cba2f96b77b3d3b08a1
Alexei-Kamaev <alexei-kamaev@yandex.ru>
2023-09-16 20:14:37 +0300

    всё

---
+++ game01.js
@@ -0,0 +1,23 @@
+'use strict';
+// вроде получилось, но с пробелами не смог номарльно обойти
+// пробелы не принимает, но кидает красную ошибку в консоли на null
+// буду рад, если скажете куда копать. уже 3 часа не могу обойти пробел
+// кроме того способа, который реализовал.
+// но при прочих равных всё работает, как надо.
+{
+const botNumber = Math.floor(Math.random() * 100);
+alert('Я загадал число от 1 до 100. Попробуй угадать...' + botNumber);
+let anyNumber = (a ='Введите число:') => {
+    let userNumber = prompt(`${a}`);
+    if (Number.isNaN(+userNumber) || userNumber == "") anyNumber();
+    if (userNumber.includes(' ')) anyNumber();
+    if (userNumber > null) resultGame(userNumber);
+    return userNumber;
+};
+const resultGame = (a, b=botNumber) => {
+    if (b > a) anyNumber('Больше!');
+    if (b < a) anyNumber('Меньше!');
+    if (b == a) return alert('Вы угадали. Поздравляю!');
+}
+anyNumber();
+}

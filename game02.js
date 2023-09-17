9a831fe6a3b0921acc43c279bf06c90c52ab8bd5
Alexei-Kamaev <alexei-kamaev@yandex.ru>
2023-09-17 13:37:22 +0300

    вторая задача

--- game02.js
+++ game02.js
@@ -1,15 +1,24 @@
 'use strict';
 {
-const minUser = () => {
-    return parseFloat(prompt('Введите начало диапазона:'))
+const arrNumbers = [];
+let minNumber = () => {
+    let userNumber = prompt(`Введите первое число:`);
+    if (Number.isNaN(+userNumber) || userNumber == "") minNumber();
+    if (userNumber.includes(' ')) minNumber();
+    if (userNumber > null) console.log(userNumber);
+    return +userNumber;
+};
+let maxNumber = () => {
+    let userNumber = prompt(`Введите второе число:`);
+    if (Number.isNaN(+userNumber) || userNumber == "") maxNumber();
+    if (userNumber.includes(' ')) maxNumber();
+    if (userNumber > null) console.log(userNumber);
+    return +userNumber;
+};
+const getRandom = (a, b) => {
+    return Math.floor(Math.random() * (Math.max(a, b) - Math.min(a, b)) + Math.min(a, b));
 }
-const maxUser = () => {
-    return parseFloat(prompt('Введите конечное число диапазона:'))
-}
-console.log(`Начало диапазона ${minUser()}, конец диапазона ${maxUser()}.`);
-
-
-
+console.log(getRandom(minNumber(), maxNumber()));
 
 
 
